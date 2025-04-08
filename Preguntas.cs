ff4fab5a61ea41b1a06756bc1c2e1dfeb60fc040
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-07 22:47:49 -0500

    Creación de la clase abstracta Preguntas, acá definimos las propiedas, Texto, Opciones, RespuestaCorrecta, Categoria y Nivel que van hacer usados en las clases hijas, tambien los metodos obtenerPuntaje y obtenerTiempoLimite que estos van hacer usados según com lo necesite las clases hijas según el nivel de pregunta y el metodo EvuluarRespuesta para que devuelva un true o false según la respuesta del usuario

---
+++ Preguntas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Pro
+{
+    public abstract class Preguntas
+    {
+        public string Texto { get; protected set; }
+        public List<string> Opciones { get; protected set; }
+        public string RespuestaCorrecta { get; protected set; }
+        public string Categoria { get; protected set; }
+        public string Nivel { get; protected set; }
+        public abstract int ObtenerPuntaje();
+        public abstract int ObtenerTiempoLimite();
+
+        public bool EvaluarRespuesta(string respuesta)
+        {
+            return respuesta == RespuestaCorrecta;
+        }
+    }
+}
