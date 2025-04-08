bd45e6d05934632088334652f4858e50cb77bbe7
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-08 00:39:23 -0500

    se crea clase Preguntafacil que retorna el puntaje y tiempo según el nivel de dificultad en esta sería fácil, implementación de polimorfismo y herencia que se traer las propiedas de la clase abstracta Preguntas

---
+++ PreguntaFacil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Pro
+{
+    public class PreguntaFacil : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 10;
+        }
+
+        public override int ObtenerTiempoLimite()
+        {
+            return 30;
+        }
+    }
+}
