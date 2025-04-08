137553b11fbf784a15a326d963b7cd1764d6f649
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-08 00:41:45 -0500

    creación clase PreguntaMedia

---
+++ PreguntaMedia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Pro
+{
+    public class PreguntaMedia : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 20;
+        }
+
+        public override int ObtenerTiempoLimite()
+        {
+            return 20;
+        }
+    }
+  
+}
