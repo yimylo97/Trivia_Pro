5f95fae33053105b6990671480eadce9c3d40b05
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-08 00:43:01 -0500

    se crea la clase PreguntaDificil

---
+++ PreguntaDificil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Pro
+{
+    public class PreguntaDificil : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 30;
+        }
+        public override int ObtenerTiempoLimite()
+        {
+            return 10;
+        }
+    }
+}
